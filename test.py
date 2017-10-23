579ee19e969dbf9f2d148dd42b1cf206a4005a2f
arash Eghtesadi <arash_eg@yahoo.com>
2017-10-22 21:16:47 -0700

    adding random code

---
+++ test.py
@@ -0,0 +1,20 @@
+print 2 ** 3
+
+str= 'hello'
+print str[-2]
+
+l = [1, 2, 3]
+print l[2]
+
+l.remove(1)
+print l
+
+t = ("hello", 1, 3)
+print t
+
+d = {
+    'name': "Arash",
+    'city': "Montreal",
+    'province': "Quebec"
+    }
+print d
