9fa45ba3074b0d55495f7486f9b632ee49ea579c
arash Eghtesadi <arash_eg@yahoo.com>
2017-11-06 19:33:04 -0800

    added more code

--- test.py
+++ test.py
@@ -1,77 +1,77 @@
-# print 2 ** 3
-#
-# #----------------------------------------------------------------------------------------------------
-#
-# str= 'hello'
-# print str[-2]
-#
-# #----------------------------------------------------------------------------------------------------
-#
-# l = [1, 2, 3]
-# print l[2]
-#
-# l.remove(1)
-# print l
-#
-# #----------------------------------------------------------------------------------------------------
-#
-# t = ("hello", 1, 3)
-# print t
-#
-# #----------------------------------------------------------------------------------------------------
-#
-# d = {
-#     'name': "Arash",
-#     'city': "Montreal",
-#     'province': "Quebec"
-#     }
-# print d
-# print 'hi'
-#
-# #----------------------------------------------------------------------------------------------------
-#
-#
-# def divide(a,b):
-#     try:
-#         return a/b
-#     except ZeroDivisionError:
-#         return "Zero division"
-#
-# print divide(300 , 10)
-#
-# #----------------------------------------------------------------------------------------------------
-#
-# age = input("enter your age")
-# print age
-#
-# #----------------------------------------------------------------------------------------------------
-#
-# names = ["Alex", "John", "Mike"]
-# for name in names:
-#     if 'A' in name:
-#         print name
-#
-# #----------------------------------------------------------------------------------------------------
-#
-# def currency_converter(rate, euros):
-#     dollars = euros * rate
-#     return dollars
-#
-# r = input("enter rate: ")
-# e = input("enter euros: ")
-# print currency_converter(float(r), float(e))
-#
-# #----------------------------------------------------------------------------------------------------
-#
-# password = ''
-# while password != 'python123':
-#     password = input("enter password : ")
-#     if password == 'python123':
-#         print "you are logged in"
-#     else:
-#         print "sorry, try again"
-#
-# #----------------------------------------------------------------------------------------------------
+print 2 ** 3
+
+#----------------------------------------------------------------------------------------------------
+
+str= 'hello'
+print str[-2]
+
+#----------------------------------------------------------------------------------------------------
+
+l = [1, 2, 3]
+print l[2]
+
+l.remove(1)
+print l
+
+#----------------------------------------------------------------------------------------------------
+
+t = ("hello", 1, 3)
+print t
+
+#----------------------------------------------------------------------------------------------------
+
+d = {
+    'name': "Arash",
+    'city': "Montreal",
+    'province': "Quebec"
+    }
+print d
+print 'hi'
+
+#----------------------------------------------------------------------------------------------------
+
+
+def divide(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError:
+        return "Zero division"
+
+print divide(300 , 10)
+
+#----------------------------------------------------------------------------------------------------
+
+age = input("enter your age")
+print age
+
+#----------------------------------------------------------------------------------------------------
+
+names = ["Alex", "John", "Mike"]
+for name in names:
+    if 'A' in name:
+        print name
+
+#----------------------------------------------------------------------------------------------------
+
+def currency_converter(rate, euros):
+    dollars = euros * rate
+    return dollars
+
+r = input("enter rate: ")
+e = input("enter euros: ")
+print currency_converter(float(r), float(e))
+
+#----------------------------------------------------------------------------------------------------
+
+password = ''
+while password != 'python123':
+    password = input("enter password : ")
+    if password == 'python123':
+        print "you are logged in"
+    else:
+        print "sorry, try again"
+
+#----------------------------------------------------------------------------------------------------
 
 names = ["Alex", "John", "Mike"]
 email_domain = ['gmail', 'yahoo', 'hotmail']
@@ -107,3 +107,23 @@ y.getdata()
 
 #----------------------------------------------------------------------------------------------------
 
+class student():
+    def student_name(self, name):
+        self.name = name
+    def qualification(self):
+        return 'student'
+    def display(self):
+        print self.qualification()
+
+# inheritance
+class eng_student(student):
+    def qualification(self):
+        return 'engineering student'
+
+bob = student()
+bob.student_name('Bob')
+bob.display()
+
+mike = eng_student()
+mike.student_name('Mike')
+mike.display()
